d14f80d9099d334d8ac418d8ffbef834d95db35f
MRMikaelJ <mr.mikael.j@hotmail.com>
2016-04-18 00:15:49 +0200

    Updates Using TestData

--- SimpleAR/src/main/java/org/opencv/samples/tutorial1/TestData.java
+++ SimpleAR/src/main/java/org/opencv/samples/tutorial1/TestData.java
@@ -30,6 +30,8 @@ public class TestData extends Activity {
     private static Size mPatternSize = new Size(4, 11);
     private static int mCornersSize = (int)(mPatternSize.width * mPatternSize.height);
     private MatOfPoint2f mCorners = new MatOfPoint2f();
+    private Mat mCameraMatrix = new Mat();
+    private Mat mDistortionCoefficients = new Mat();
     static {
         if (!OpenCVLoader.initDebug()) {
             // Handle initialization error
@@ -77,7 +79,18 @@ public class TestData extends Activity {
         //    mPatternWasFound = Calib3d.findChessboardCorners(grayFrame,mPatternSize,mCorners,Calib3d.CALIB_CB_FAST_CHECK);
     }
 
+   /* File root = Environment.getExternalStorageDirectory();
+    ImageView IV = (ImageView) findViewById(R.id."image view");
+    Bitmap bMap = BitmapFactory.decodeFile(root+"/images/01.jpg");
+    IV.setImageBitmap(bMap);*/
+
     public void ImageDrawTest() {
+        // MakeCamera Image
+        Mat.eye(3, 3, CvType.CV_64FC1).copyTo(mCameraMatrix);
+        mCameraMatrix.put(0, 0, 1.0);
+        Mat.zeros(5, 1, CvType.CV_64FC1).copyTo(mDistortionCoefficients);
+        //Assume UnDistorted
+
         // make a mat and draw something
         Mat m = Mat.zeros(645, 501, CvType.CV_8UC3);
         Mat gray = Mat.zeros(645, 501, CvType.CV_8UC1);
@@ -87,16 +100,19 @@ public class TestData extends Activity {
         Utils.bitmapToMat(bMap, m);
         Imgproc.cvtColor(m, gray, Imgproc.COLOR_BGR2GRAY);
         boolean contains = findPattern(gray);
+        Mat over = Mat.zeros(74, 67, CvType.CV_8UC3);
+        Bitmap xMap = BitmapFactory.decodeResource(res, R.drawable.overlay);  // OverLay 2-D Image
+        Utils.bitmapToMat(xMap, over);
         if (contains) {
             Imgproc.putText(m, "Pattern Detected", new Point(30, 80), Core.FONT_HERSHEY_SCRIPT_SIMPLEX, 2.2, new Scalar(200, 200, 0), 2);
-            //
+            //Now The Corners Of Figure is in mCorner
+            Point[] points = mCorners.toArray();
+            over.copyTo(m.colRange((int) points[0].x, (int) points[0].x + 74).rowRange((int)points[0].y, (int)points[0].y + 67));
+
         }else{
             Imgproc.putText(m, "Pattern Undetected", new Point(30, 80), Core.FONT_HERSHEY_SCRIPT_SIMPLEX, 2.2, new Scalar(200, 200, 0), 2);
+            over.copyTo(m.colRange(80, 80 + 74).rowRange(100, 100 + 67));
         }
-            Bitmap xMap = BitmapFactory.decodeResource(res, R.drawable.overlay);  // OverLay 2-D Image
-        Mat over = Mat.zeros(74, 67, CvType.CV_8UC3);
-        Utils.bitmapToMat(xMap,over);
-        over.copyTo(m.colRange(80, 80 + 74).rowRange(100, 100 + 67));
         Bitmap bm = Bitmap.createBitmap(m.cols(), m.rows(), Bitmap.Config.ARGB_8888);
         Utils.matToBitmap(m, bm);
         // find the imageview and draw it!
